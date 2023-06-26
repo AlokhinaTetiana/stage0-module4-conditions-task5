4fd3cbe51c7a0ecc75fb09009493e5e19662e174
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-06-27 00:51:57 +0300

    j

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int check;
+        if(year%4==0&&year%100!=0 || year%4==0&&year%100==0&&year%400==0) check=0;
+        else{check=1;};
+        switch (check){
+            case 0:System.out.println("leap");
+                break;
+            default: System.out.println("not leap");
+        }
     }
 }
